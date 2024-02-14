94c003eadd4a7198461160c99df70213fb0e3ece
Aubrey Whitford <Aubreya.whitford@gmail.com>
2024-02-14 15:24:50 -0500

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+saturdayFun()
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+mondayWork()
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
